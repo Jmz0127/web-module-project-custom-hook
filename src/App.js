921c17c13bd544a0c0fdb1c84b78934d0885e2c3
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-01 16:38:25 -0500

    built useDarkMode hook and passed into App.js. Toggle functions correctly. Not localstorage functionality..yet

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
+import Charts from './components/Charts';
+import Navbar from './components/Navbar';
 
-import "./style.css";
+import './style.css';
+import useDarkMode from './hooks/useDarkMode';
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+	const [coinData, setCoinData] = useState([]);
+	const [useDarkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
-  return (
-    <div className={darkMode ? "dark-mode App" : "App"}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-      <Charts coinData={coinData} />
-    </div>
-  );
+	useEffect(() => {
+		axios
+			.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true')
+			.then((res) => setCoinData(res.data))
+			.catch((err) => console.log(err));
+	}, []);
+	return (
+		<div className={useDarkMode ? 'dark-mode App' : 'App'}>
+			<Navbar darkMode={useDarkMode} setDarkMode={setDarkMode} />
+			<Charts coinData={coinData} />
+		</div>
+	);
 };
 
 export default App;
